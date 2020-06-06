cb24087db57b1b30445ff1a1e31c3debbc7fc2f9
gama79530 <gama79530@gmail.com>
2020-06-07 01:10:17 +0800

    Decorator Pattern

---
+++ DecoratorPattern/Beverage.py
@@ -0,0 +1,81 @@
+"""
+    All concrete classes should extend Beverage.
+
+    All concrete classes should overide the following attributes.
+    description = None
+
+    All concrete classes should implement the following methods.
+    def cost(self) :
+"""
+class Beverage :
+    description = None
+
+    def getDescription(self) :
+        return self.description
+
+class Espresso(Beverage) :
+    description = 'Espresso'
+
+    def cost(self) :
+        return 1.99
+
+class HouseBlend(Beverage) :
+    description = 'House Blend Coffee'
+
+    def cost(self) :
+        return 0.89
+
+class DarkRoast(Beverage) :
+    description = 'Dark Roast Coffee'
+
+    def cost(self) :
+        return 0.99
+        
+class Decaf(Beverage) :
+    description = 'Decaf'
+
+    def cost(self) :
+        return 1.05
+
+"""
+    All decorator classes should extend Condiment.
+
+    All decorator classes should override or implement the following methods.
+    def getDescription(self) :
+    def cost(self) :
+"""
+
+class Condiment(Beverage) :
+    beverage = None
+
+    def __init__(self, beverage) :
+        self.beverage = beverage
+
+class Milk(Condiment) :
+    def getDescription(self) :
+        return self.beverage.getDescription() + ', Milk'
+
+    def cost(self) :
+        return 0.1 + self.beverage.cost()
+
+class Mocha(Condiment) :
+    def getDescription(self) :
+        return self.beverage.getDescription() + ', Mocha'
+        
+    def cost(self) :
+        return 0.2 + self.beverage.cost()
+
+class Soy(Condiment) :
+    def getDescription(self) :
+        return self.beverage.getDescription() + ', Soy'
+        
+    def cost(self) :
+        return 0.15 + self.beverage.cost()
+        
+
+class Whip(Condiment) :
+    def getDescription(self) :
+        return self.beverage.getDescription() + ', Whip'
+        
+    def cost(self) :
+        return 0.1 + self.beverage.cost()
