7575501763b48a74da195d3699cb404432043287
gama79530 <gama79530@gmail.com>
2020-06-09 15:49:42 +0800

    update Decorator Pattern

--- DecoratorPattern/Beverage.py
+++ DecoratorPattern/Beverage.py
@@ -1,51 +1,54 @@
 """
-    All concrete classes should extend Beverage.
+    # All concrete classes should extend Beverage.
 
-    All concrete classes should overide the following attributes.
-    description = None
+    # All concrete classes should overide the following attributes.
+    self.description = None
 
-    All concrete classes should implement the following methods.
+    # All concrete classes should implement the following methods.
     def cost(self) :
 """
 class Beverage :
-    description = None
+    def __init__(self) :
+        self.description = None
 
     def getDescription(self) :
         return self.description
 
 class Espresso(Beverage) :
-    description = 'Espresso'
+    def __init__(self) :
+        self.description = 'Espresso'
 
     def cost(self) :
         return 1.99
 
 class HouseBlend(Beverage) :
-    description = 'House Blend Coffee'
+    def __init__(self) :
+        self.description = 'House Blend Coffee'
 
     def cost(self) :
         return 0.89
 
 class DarkRoast(Beverage) :
-    description = 'Dark Roast Coffee'
+    def __init__(self) :
+        self.description = 'Dark Roast Coffee'
 
     def cost(self) :
         return 0.99
         
 class Decaf(Beverage) :
-    description = 'Decaf'
+    def __init__(self) :
+        self.description = 'Decaf'
 
     def cost(self) :
         return 1.05
 
 """
-    All decorator classes should extend Condiment and override the following methods.
+    # All decorator classes should extend Condiment and override the following methods.
     def getDescription(self) :
     def cost(self) :
 """
 
 class Condiment(Beverage) :
-    beverage = None
-
     def __init__(self, beverage) :
         self.beverage = beverage
 
@@ -62,21 +65,20 @@ class Milk(Condiment) :
 class Mocha(Condiment) :
     def getDescription(self) :
         return self.beverage.getDescription() + ', Mocha'
-        
+
     def cost(self) :
         return 0.2 + self.beverage.cost()
 
 class Soy(Condiment) :
     def getDescription(self) :
         return self.beverage.getDescription() + ', Soy'
-        
+
     def cost(self) :
         return 0.15 + self.beverage.cost()
         
-
 class Whip(Condiment) :
     def getDescription(self) :
         return self.beverage.getDescription() + ', Whip'
-        
+
     def cost(self) :
         return 0.1 + self.beverage.cost()
--- DecoratorPattern/DecoratorPattern.md
+++ DecoratorPattern/DecoratorPattern.md
@@ -1,4 +1,7 @@
 # Decorator Pattern
 
 ## Tips
-1. 
+1. Classes should be open for extension, but closed for modification.
+
+## Summary
+1. Have base objects and a lot of optional plug-in objects.
