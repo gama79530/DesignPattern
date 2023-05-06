bdef2502fd1d4bf45c7de5fe2d65349bb2b118eb
Rein, Wu <gama79530@gmail.com>
2023-05-06 08:20:01 +0800

    Modify: add rvalue reference constructor

--- DecoratorPattern/cpp/main.cpp
+++ DecoratorPattern/cpp/main.cpp
@@ -6,14 +6,10 @@
 using namespace std;
 
 int main(){
-{   HouseBlend houseBlend;
-    Milk milk(houseBlend);
-    Mocha mocha(milk);
-    Beverage &beverage = mocha;
+{   Beverage &&beverage = Mocha(Milk(HouseBlend()));
     cout << "HouseBlend Coffee with mocha and milk: price = " << to_string(beverage.getPrice()) << ", cost = " << to_string(beverage.getCost()) << ", profit = " << to_string(beverage.getProfit()) << endl;
 }
-{   DarkRoast darkRoast;
-    Beverage &beverage = darkRoast;
+{   Beverage &&beverage = DarkRoast();
     cout << "DarkRoast Coffee without any condiment: price = " << to_string(beverage.getPrice()) << ", cost = " << to_string(beverage.getCost()) << ", profit = " << to_string(beverage.getProfit()) << endl;
 }
     return 0;
--- DecoratorPattern/cpp/src/header/condiment.h
+++ DecoratorPattern/cpp/src/header/condiment.h
@@ -9,6 +9,7 @@ class Condiment: public Beverage{
 class Milk: public Condiment{
 public:
     Milk(Beverage& beverage){this->beverage = &beverage;}
+    Milk(Beverage&& beverage){this->beverage = &beverage;}
     int getPrice(){return this->beverage->getPrice() + 10;}
     int getCost(){return this->beverage->getCost() + 2;}
     int getProfit(){return getPrice() - getCost();}
@@ -19,6 +20,7 @@ private:
 class Mocha: public Condiment{
 public:
     Mocha(Beverage& beverage){this->beverage = &beverage;}
+    Mocha(Beverage&& beverage){this->beverage = &beverage;}
     int getPrice(){return this->beverage->getPrice() + 15;}
     int getCost(){return this->beverage->getCost() + 5;}
     int getProfit(){return getPrice() - getCost();}
